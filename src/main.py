f0139fbe0d0d30cfc77edc66677e67fc294bb303
willian365x <willian365x@gmail.com>
2025-05-09 17:08:10 -0300

    Aplicando Clean Code

--- src/main.py
+++ src/main.py
@@ -1,46 +1,23 @@
-import os
 import flet as ft
-from dotenv import load_dotenv
-
-from controles import Calculo
-
-load_dotenv()
-
+from controles import AppBarra, Calculo
 
 def main(page: ft.Page):
     page.title = 'Preço Médio de Ações'
-
     # Controles
+    app_barra = AppBarra('Preço Médio', icon=ft.Icons.CURRENCY_EXCHANGE)
     calculo = Calculo()
-    txt_quantidade = ft.Text()
-    txt_preco = ft.Text()
-    btn_add_calc = ft.ElevatedButton('Adicionar',
-                                     icon=ft.Icons.ADD,
-                                     on_click=lambda e: page.open(calculo),
-                                     )
 
     # Action
-    def on_close_calculo(e):
-        load_dotenv(override=True)
-        txt_quantidade.value = f"Quantidade: {os.environ.get('QUANTIDADE')}"
-        txt_preco.value = f'Preço: {os.environ.get('PRECO')}'
-        print('Dialog BootoSheet foi fechado.')
-        page.update()
-
-    calculo.on_dismiss = on_close_calculo
-
     page.add(
         ft.SafeArea(
             ft.Column(
                 [
-                    btn_add_calc,
-                    txt_quantidade,
-                    txt_preco
                 ]
             )
         )
     )
 
+    page.appbar = app_barra
     page.overlay.append(calculo)
     page.update()
 
